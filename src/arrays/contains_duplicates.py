6f05ab7962dc9b4f02f3ecc76b00ed502db51a6a
Ihor Plakhotniuk <Ihor.P@MacBook-Pro-Ihor.local>
2026-03-21 17:16:05 +0200

    added 2 version of contains duplicates problem

--- src/arrays/contains_duplicates.py
+++ src/arrays/contains_duplicates.py
@@ -2,7 +2,29 @@ def contains_duplicates_217_v1(nums: list[int]) -> bool:
     return len(nums) != len(set(nums))
 
 
+def contains_duplicates_217_v2(nums: list[int]) -> bool:
+    nums.sort()
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return True
+    return False
+
+
+def contains_duplicates_217_v3(nums: list[int]) -> bool:
+    s = set()
+    for i in range(len(nums)):
+        if nums[i] in s:
+            return True
+        s.add(nums[i])
+    return False
+
+
 if __name__ == "__main__":
     print(contains_duplicates_217_v1([1, 2, 3, 4]))  # False
     print(contains_duplicates_217_v1([1, 2, 3, 3]))  # True
-    print(contains_duplicates_217_v1([]))  # False
+
+    print(contains_duplicates_217_v2([1, 2, 3, 4]))
+    print(contains_duplicates_217_v2([1, 2, 3, 3]))
+
+    print(contains_duplicates_217_v3([1, 2, 3, 4]))
+    print(contains_duplicates_217_v3([1, 2, 3, 3]))
--- tests/arrays/test_contains_duplicates.py
+++ tests/arrays/test_contains_duplicates.py
@@ -1,6 +1,10 @@
 import pytest
 
-from src.arrays.contains_duplicates import contains_duplicates_217_v1
+from src.arrays.contains_duplicates import (
+    contains_duplicates_217_v1,
+    contains_duplicates_217_v2,
+    contains_duplicates_217_v3,
+)
 
 
 @pytest.mark.parametrize(
@@ -15,3 +19,31 @@ from src.arrays.contains_duplicates import contains_duplicates_217_v1
 )
 def test_contains_duplicates_v1(nums: list[int], expected: bool) -> None:
     assert contains_duplicates_217_v1(nums) == expected
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 4], False),
+        ([1, 2, 3, 3], True),
+        ([10], False),
+        ([], False),
+        ([1, 1, 1, 1, 2, 2, 3, 3, 4, 1], True),
+    ],
+)
+def test_contains_duplicates_v2(nums: list[int], expected: bool) -> None:
+    assert contains_duplicates_217_v2(nums) == expected
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 4], False),
+        ([1, 2, 3, 3], True),
+        ([10], False),
+        ([], False),
+        ([1, 1, 1, 1, 2, 2, 3, 3, 4, 1], True),
+    ],
+)
+def test_contains_duplicates_v3(nums: list[int], expected: bool) -> None:
+    assert contains_duplicates_217_v3(nums) == expected
